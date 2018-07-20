da3601cae51d7044cd26b8c5d1cd423a4cd38161
maxwellharon <maxwellharon54@gmail.com>
2018-07-20 19:04:50 +0300

    Initial commit

---
+++ maxgram/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MaxgramConfig(AppConfig):
+    name = 'maxgram'
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/max/Documents/moringa-school-projects/Instamax/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
