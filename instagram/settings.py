aff6812fc47471182c2268e93375de7c9fa61d3a
maxwellharon <maxwellharon54@gmail.com>
2018-07-22 14:03:06 +0300

    Add db in settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'instamax',
+        'NAME': 'instagram2',
         'USER': 'max',
         'PASSWORD': '1234',
     }
