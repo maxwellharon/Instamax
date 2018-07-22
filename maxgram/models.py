698c4c914fa46ce82a8f7e18a9d82810bf395365
maxwellharon <maxwellharon54@gmail.com>
2018-07-22 14:05:04 +0300

    Create  class profile for registration of profile picture

--- maxgram/models.py
+++ maxgram/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Profile(models.Model):
+    profilePic = models.ImageField(upload_to='profile/',null=True,blank=True)
+    bio = models.CharField(max_length=60,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
 
 # Create your models here.
