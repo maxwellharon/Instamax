f4da40f4bfd2402c5c10ae6c55a226b4a9ad1629
maxwellharon <maxwellharon54@gmail.com>
2018-07-22 14:07:42 +0300

    Addfunction for saving profile

--- maxgram/models.py
+++ maxgram/models.py
@@ -9,5 +9,13 @@ class Profile(models.Model):
     bio = models.CharField(max_length=60,blank=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
 
 # Create your models here.
